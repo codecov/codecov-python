a88435b174dff24c4dd0809c3d16e960784c3285
stevepeak <steve@stevepeak.net>
2014-09-12 07:46:27 -0400

    version++

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-version = '0.2.2'
+version = '0.2.3'
 classifiers = ["Development Status :: 4 - Beta",
                "Environment :: Plugins",
                "Intended Audience :: Developers",
