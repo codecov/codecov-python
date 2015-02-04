f28a9c6b524a37769aa7de9fe8c83a0ef46a6df1
stevepeak <steve@stevepeak.net>
2015-02-04 18:20:08 -0500

    fix 2.6

--- codecov/clover.py
+++ codecov/clover.py
@@ -6,16 +6,16 @@ IGNORE = re.compile(r'^((vendor/)|(\$\{))')
 
 def from_xml(xml):
     coverage = {}
-    for f in xml.iter('file'):
+    for f in getattr(xml, "iter", "findall")('file'):
         path = f.attrib['name']
         if IGNORE.match(path):
             continue
         elif f.find('line') is None:
             continue
 
-        lines = [None]*(max([int(line.attrib['num']) for line in f.iter('line')] or [0])+1)
+        lines = [None]*(max([int(line.attrib['num']) for line in getattr(f, 'iter', 'findall')('line')] or [0])+1)
         if len(lines) > 1:
-            for line in f.iter('line'):
+            for line in getattr(f, 'iter', 'findall')('line'):
                 lines[int(line.attrib['num'])] = int(line.attrib['count'] or 0)
         lines[0] = None
         # skip last line
--- codecov/cobertura.py
+++ codecov/cobertura.py
@@ -5,18 +5,18 @@ IGNORE = re.compile(r"^\/home\/travis\/virtualenv\/python")
 
 def from_xml(xml):
     coverage, branches = {}, {}
-    for _class in xml.iter('class'):
+    for _class in getattr(xml, 'iter', 'findall')('class'):
         # extract file name
         filename = _class.attrib['filename']
         if IGNORE.match(filename):
             # ignore this all together
             continue
 
-        lines = [None]*(max([int(line.attrib['number']) for line in _class.iter('line')] or [0])+1)
+        lines = [None]*(max([int(line.attrib['number']) for line in getattr(_class, 'iter', 'findall')('line')] or [0])+1)
         if len(lines) == 1:
             continue
 
-        for line in _class.iter('line'):
+        for line in getattr(_class, 'iter', 'findall')('line'):
             cc = str(line.attrib.get('condition-coverage', ''))
             lines[int(line.attrib['number'])] = cc.split(' ',1)[1][1:-1] if cc else int(line.attrib.get('hits', 0) or 0)
         coverage[filename] = lines
