3a898f9da99f3adee2b6eb897d6c1e5847591f29
stevepeak <steve@stevepeak.net>
2015-06-12 09:03:18 -0400

    v++

--- codecov/__init__.py
+++ codecov/__init__.py
@@ -33,7 +33,7 @@ try:
 except:
     pass
 
-version = VERSION = __version__ = '1.1.9'
+version = VERSION = __version__ = '1.1.10'
 
 SKIP_DIRECTORIES = re.compile(r'\/(\..+|((Sites\/www\/bower)|node_modules|vendor|bower_components|(coverage\/instrumented)|\.?virtualenv|venv\/(lib|bin)|build\/lib|\.git|\.egg\-info))\/').search
 SKIP_FILES = re.compile(r'(\.tar\.gz|\.pyc|\.egg|(\/\..+)|\.txt)$').search
