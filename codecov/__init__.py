a5ee9245fef09fb66c683de544e5363da4bdb744
Steve Peak <steve@stevepeak.net>
2016-07-10 11:57:37 -0400

    add agnostic variable detection
    https://github.com/codecov/codecov-python/issues/68

--- codecov/__init__.py
+++ codecov/__init__.py
@@ -456,6 +456,14 @@ def main(*argv, **kwargs):
 
             write('    Gitlab CI Detected')
 
+        else:
+            query.update(dict(commit=os.getenv('VCS_COMMIT_ID', ''),
+                              branch=os.getenv('VCS_BRANCH_NAME', ''),
+                              build_url=os.getenv('VCS_BRANCH_URL', ''),
+                              build=os.getenv('VCS_BUILD_ID', ''),
+                              pr=os.getenv('VCS_PULL_REQUEST', ''),
+                              slug=os.getenv('VCS_SLUG', '')))
+
         # ------
         # git/hg
         # ------
