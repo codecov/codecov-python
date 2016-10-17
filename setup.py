7386c3c13fc71f237611c780646fa1b44a07dc64
Steve Peak <steve@codecov.io>
2016-10-17 07:10:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(name='codecov',
       packages=['codecov'],
       include_package_data=True,
       zip_safe=True,
-      install_requires=["requests>=2.0.0", "coverage", "argparse"],
+      install_requires=["requests>=2.7.9", "coverage", "argparse"],
       tests_require=["unittest2"],
       entry_points={'console_scripts': ['codecov=codecov:main']})
