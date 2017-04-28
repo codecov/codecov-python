7ab5e9325ce9cbf0b94019e5ad062d3292243631
Steve Peak <steve@stevepeak.net>
2017-04-28 10:06:12 -0400

    v++

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 import sys
 
-version = '2.0.8'
+version = '2.0.9'
 classifiers = ["Development Status :: 5 - Production/Stable",
                "Environment :: Plugins",
                "Intended Audience :: Developers",
