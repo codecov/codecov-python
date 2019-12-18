978c5be28f06dffcd2f95829fe8e9a8291d5146a
Zach Kauffman <zach@getsidecar.com>
2019-12-18 13:08:27 -0500

    Pin coverage to < 5

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(name='codecov',
       packages=['codecov'],
       include_package_data=True,
       zip_safe=True,
-      install_requires=["requests>=2.7.9", "coverage"],
+      install_requires=["requests>=2.7.9", "coverage<5"],
       entry_points={'console_scripts': ['codecov=codecov:main']},
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
       )
