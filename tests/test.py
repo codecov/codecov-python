73472e5eb6abea7550db4cda42201cae206d8514
Thomas Hu <tomhu1096@gmail.com>
2020-06-13 11:54:16 -0400

    Fixes

--- tests/test.py
+++ tests/test.py
@@ -888,8 +888,8 @@ class TestUploader(unittest.TestCase):
         res = self.run_cli()
         self.assertEqual(res["query"]["service"], "github-actions")
         self.assertEqual(res["query"]["commit"], os.getenv("GITHUB_SHA"))
-        # self.assertEqual(res["query"]["build"], "1399372237")
-        # self.assertEqual(res["query"]["slug"], "owner/repo")
+        self.assertEqual(res["query"]["build"], "1399372237")
+        self.assertEqual(res["query"]["slug"], "owner/repo")
         # self.assertEqual(res["codecov"].token, "token")
         # self.assertEqual(res["codecov"].name, "name")
 
