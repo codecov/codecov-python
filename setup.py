5862842228187f4ed919bf8f9c1ac3692d1ccbbc
Gaurav Sheni <gvsheni@gmail.com>
2020-10-16 10:07:28 -0400

    Update to Python 3.9 (#301)
    * Update .travis.yml
    
    * Update setup.py
    
    * Update build-test.yml
    
    * Update .travis.yml
    
    Co-authored-by: Tom Hu <thomasrockhu@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ classifiers = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: Implementation :: PyPy",
     "License :: OSI Approved :: Apache Software License",
     "Topic :: Software Development :: Testing",
